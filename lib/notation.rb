492eaf8b4b47fba03880ca468b07960587a59eb0
Daniel Berger <dberger@globe.gov>
2009-12-13 17:13:10 -0700

    Cosmetic changes to lib.

--- lib/notation.rb
+++ lib/notation.rb
@@ -1,30 +1,30 @@
 # Run with -Ku
 
 module Kernel
-   # Make lambda a true lambda
-   #
-   # Example:
-   #    λ { puts 'Hello' }.call => 'Hello'
-   #    
-   alias λ proc
+  # Make lambda a true lambda
+  #
+  # Example:
+  #    λ { puts 'Hello' }.call => 'Hello'
+  #    
+  alias λ proc
    
-   # Sigma, i.e. the sum of all elements.
-   #
-   # Example:
-   #    ∑ [1,2,3] => 6
-   #
-   def ∑(*args)
-      sum = 0
-      args.each{ |e| sum += e }
-      sum
-   end
+  # Sigma, i.e. the sum of all elements.
+  #
+  # Example:
+  #    ∑ [1,2,3] => 6
+  #
+  def ∑(*args)
+    sum = 0
+    args.each{ |e| sum += e }
+    sum
+  end
    
-   # Square root
-   #
-   # Example:
-   #   √ 49 => 7.0
-   #  
-   def √(root)
-      Math.sqrt(root)
-   end
+  # Square root
+  #
+  # Example:
+  #   √ 49 => 7.0
+  #  
+  def √(root)
+    Math.sqrt(root)
+  end
 end

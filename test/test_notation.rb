4e7ca7efe69f84a451acb738a11e2038ae954136
Daniel Berger <dberger@globe.gov>
2009-12-13 17:25:08 -0700

    Changed test case name.

--- test/test_notation.rb
+++ test/test_notation.rb
@@ -7,7 +7,7 @@
 require 'test/unit'
 require 'notation'
 
-class TC_Fortress < Test::Unit::TestCase
+class TC_Notation < Test::Unit::TestCase
   def test_sigma
     assert_respond_to(Kernel, :∑)
     assert_equal(6, ∑(1,2,3))

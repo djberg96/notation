9a634c1666c16279a1ce8953b645a8f1b3ee2121
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-06-22 18:38:01 -0400

    Apply Style/StringLiterals cop.

--- spec/notation_spec.rb
+++ spec/notation_spec.rb
@@ -8,28 +8,28 @@
 require 'rspec'
 require 'notation'
 
-RSpec.describe "Notation" do
-  example "version" do
+RSpec.describe 'Notation' do
+  example 'version' do
     expect(Kernel::NOTATION_VERSION).to eq('0.2.1')
     expect(Kernel::NOTATION_VERSION).to be_frozen
   end
 
-  example "sigma" do
+  example 'sigma' do
     expect(Kernel).to respond_to(:∑)
     expect(∑(1, 2, 3)).to eq(6)
   end
 
-  example "pi" do
+  example 'pi' do
     expect(Kernel).to respond_to(:∏)
     expect(∏(2, 3, 4)).to eq(24)
   end
 
-  example "square_root" do
+  example 'square_root' do
     expect(Kernel).to respond_to(:√)
     expect(√(49)).to eq(7.0)
   end
 
-  example "lambda" do
+  example 'lambda' do
     expect(λ{ 'hello' }.call).to eq('hello')
   end
 end
